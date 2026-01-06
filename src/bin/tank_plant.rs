b9bf2c6f41ab7e8713a098bdab556eaee04359f6
SCool62 <samthecool300@icloud.com>
2026-01-06 10:04:43 -0800

    First run

--- src/bin/tank_plant.rs
+++ src/bin/tank_plant.rs
@@ -1,11 +1,20 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::default_serial;
+
+
 use panic_halt as _;
 
 // TODO
 #[arduino_hal::entry]
 fn main() -> ! {
+    let peripherals = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(peripherals);
+
+    let mut serial = default_serial!(peripherals, pins, 57600);
+    ufmt::uwriteln!(&mut serial, "Peripherals setup complete").unwrap();
+
     loop {
 
     }
